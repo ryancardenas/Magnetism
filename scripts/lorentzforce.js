168bf25a93a8427353761929c96ae368cf8883a1
Ryan Cardenas <31240617+ryancardenas@users.noreply.github.com>
2018-12-14 18:23:56 -0800

    I got a ball to bounce! And the canvas resize with every update cycle.

--- scripts/lorentzforce.js
+++ scripts/lorentzforce.js
@@ -1,36 +1,64 @@
-function main(){
+var main = function(){
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d')
+  var cw = canvas.width;
+  var ch = canvas.height;
+  var t0 = (new Date()).getTime();
+  var t1 = 0;
+  var delta = 0;
+  var fps = 30,
+    bX = 30,
+    bY = 30,
+    mX = 150,
+    mY = 300;
 
   //1000 ms / 60 FPS = 16.667 ms per frame
   var frameperiod = 1000 / 60;
 
-  requestAnimationFrame(gameloop);
+  window.requestAnimationFrame(gameloop);
 
   function gameloop(){
-    update();
-    draw();
-    requestAnimationFrame(gameloop);
+    t1 = (new Date()).getTime();
+    delta = (t1 - t0) / 1000;
+    update(delta);
+    t0 = t1;
+    window.requestAnimationFrame(gameloop);
   }
 
   function update() {
+    ctx.clearRect(0,0,cw,ch);
     resize(canvas);
-    ctx.fillText("Width: " + canvas.width, 10, 50);
-    ctx.fillText("Height: " + canvas.height, 10, 70);
+    ctx.fillText("Width: " + cw, 10, 50);
+    ctx.fillText("Height: " + ch, 10, 70);
     ctx.beginPath();
     ctx.moveTo(20,20);
     ctx.lineTo(200,80);
     ctx.lineTo(500,700);
     ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.arc(bX, bY, 20, 0, Math.PI * 360);
+    ctx.fill();
+    if ((bX >= cw && mX > 0) || (bX <= 0 && mX < 0)) {
+        mX *= -1;
+    }
+    if ((bY >= ch && mY > 0) || (bY <= 0 && mY < 0)) {
+        mY *= -1;
+    }
+
+    bX += (mX * delta);
+    bY += (mY * delta);
+
   }
 
   function resize(canvas) {
     var displayWidth = window.innerWidth;
     var displayHeight = window.innerHeight;
 
-    if (canvas.width != displayWidth || canvas.height != displayHeight){
-          canvas.width = displayWidth;
-          canvas.height = displayHeight;
+    if (cw != displayWidth || ch != displayHeight){
+          cw = displayWidth;
+          ch = displayHeight;
         }
   }
 }
