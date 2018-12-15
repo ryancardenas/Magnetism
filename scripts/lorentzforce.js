da60e890b61fc59815b51784b2f50f1e1ab81155
Ryan Cardenas <31240617+ryancardenas@users.noreply.github.com>
2018-12-14 17:39:11 -0800

    All functions are defined inside of main()

--- scripts/lorentzforce.js
+++ scripts/lorentzforce.js
@@ -1,22 +1,38 @@
+function main(){
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d')
 
-var canvas = document.getElementById('sim');
-var ctx = canvas.getContext("2d");
+  //1000 ms / 60 FPS = 16.667 ms per frame
+  var frameperiod = 1000 / 60;
 
-update();
-window.addEventListener('resize', resizeCanvas, false);
-window.addEventListener('orientationchange', resizeCanvas, false);
-resizeCanvas();
+  requestAnimationFrame(gameloop);
 
-function resizeCanvas() {
-  canvas.width = $("tesdiv").width();
-  canvas.height = $("#parent").height();
-  update();
+  function gameloop(){
+    update();
+    draw();
+    requestAnimationFrame(gameloop);
+  }
+
+  function update() {
+    resize(canvas);
+    ctx.fillText("Width: " + canvas.width, 10, 50);
+    ctx.fillText("Height: " + canvas.height, 10, 70);
+    ctx.beginPath();
+    ctx.moveTo(20,20);
+    ctx.lineTo(200,80);
+    ctx.lineTo(500,700);
+    ctx.stroke();
+  }
+
+  function resize(canvas) {
+    var displayWidth = window.innerWidth;
+    var displayHeight = window.innerHeight;
+
+    if (canvas.width != displayWidth || canvas.height != displayHeight){
+          canvas.width = displayWidth;
+          canvas.height = displayHeight;
+        }
+  }
 }
 
-function update() {
-  ctx.beginPath();
-  ctx.moveTo(20,20);
-  ctx.lineTo(200,80);
-  ctx.lineTo(500,700);
-  ctx.stroke();
-}
+main();
